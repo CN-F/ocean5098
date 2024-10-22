0c91563677fe0dc55b34614204c1de6e28063128
CN-F <joye867954@gmail.com>
2024-10-22 13:47:41 +0800

    correction

--- 90-Code/04.2-ggplot2.R
+++ 90-Code/04.2-ggplot2.R
@@ -1,5 +1,7 @@
-
-data(rairuoho)
+#########################
+### ggplot practice
+#########################
+### read rairuoho data
 rairuoho_path<- "C:\\ocean5098\\02.3-Rairuoho\\rairuoho_data.txt"
 rairuoho_data<- read.table(rairuoho_path, header = T)
 head(rairuoho_data,5)
