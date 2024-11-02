619c53b6f55ce7d75d668cbc2d40b64fe129035f
CN-F <joye867954@gmail.com>
2024-11-03 04:15:37 +0800

    practice 5.1 mapping

--- 90-Code/05.1-mapping.R
+++ 90-Code/05.1-mapping.R
@@ -7,24 +7,21 @@
 library(ggplot2)
 library(rgbif)
 library(dplyr)
-rm(list=ls())
-# Get data for Common House Sparrow
-species_data <- occ_data(scientificName = "Isoetes taiwanensis", 
-                         limit = 1000)
-# Check the data
-head(species_data$data)
-# Clean the data
-cleaned_data <- species_data$data %>%
-  filter(!is.na(decimalLongitude) & !is.na(decimalLatitude))
-
-# Create a static map
-ggplot(cleaned_data, aes(x = decimalLongitude, y = decimalLatitude)) +
-  geom_point(alpha = 0.5) +
-  borders("world") +
-  coord_fixed() +
-  theme_minimal() +
-  labs(title = "Distribution of Common House Sparrow (Passer domesticus)")
 library(leaflet)
+rm(list=ls())
+# dir create
+map_dir<- "C://ocean5098//05-mapping_practice"
+if (dir.exists(map_dir) == FALSE)
+{
+  dir.create(map_dir)
+}
+
+# Get data for Common House Sparrow
+species_data <- occ_search(scientificName = "Isoetes taiwanensis",
+                           hasCoordinate=T,
+                           basisOfRecord='HUMAN_OBSERVATION',
+                           limit = 1000) 
+head(species_data$data)
 
 # Create an interactive map
 leaflet(cleaned_data) %>%
